a47484c61f211b425a8621b4ee9b52042efc04fe
didiotghima <nurlanuuulubeksultan@gmail.com>
2025-03-12 19:58:07 +0600

    com

--- core/settings.py
+++ core/settings.py
@@ -201,10 +201,10 @@ JAZZMIN_SETTINGS = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Например, Gmail SMTP
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nurlanuuulubeksultan@gmail.com'  # Твой email
-EMAIL_HOST_PASSWORD = 'ztdwjizhqxbboncb'  # Пароль приложения (НЕ обычный пароль)
+EMAIL_HOST_USER = 'nurlanuuulubeksultan@gmail.com'
+EMAIL_HOST_PASSWORD = 'ztdwjizhqxbboncb'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-ADMIN_EMAIL = 'grazy9891@gmail.com'  # Кому отправлять уведомления
+ADMIN_EMAIL = 'grazy9891@gmail.com'
